396b1aa5571010d123238b608a397c16e2d9ed09
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-06-24 13:35:56 -0700

    cleaned up syntax

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,13 +3,14 @@ import './Header.css'
 import { Link } from 'react-router-dom'
 
 export default function Header(props) {
-    console.log(props, "here it is")
     return (
         <div>
-        <header className="header"
-            // onClick={() => {props.history.push("/")}}
-        >
-            <Link to={'/'}><button class="headerTitle"><h1>Noteful</h1></button></Link>
+        <header className="header">
+            <Link to={'/'}>
+                <button class="headerTitle">
+                    <h1>Noteful</h1>
+                </button>
+            </Link>
         </header>
         </div>
     )
