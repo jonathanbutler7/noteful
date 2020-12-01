ae6bd62312d62c67b9f0bbd03fd7d7cdb490344b
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-11-30 21:15:50 -0800

    added routes to store.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import FolderSelect from './Components/FolderList/FolderList';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import NoteRoute from './Components/NoteRoute/NoteRoute';
-import Error from './Components/ErrorPage/ErrorPage';
+import { routes } from './dummy-store';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
 import Header from './Components/Header/Header';
 import NotefulContext from './NotefulContext';
-import AddFolder from './Components/AddFolder/AddFolder';
 import NoteDetails from './Components/NoteDetails/NoteDetails';
-import AddNote from './Components/AddNote/AddNote';
 import ErrorBoundary from './Components/ErrorBoundary/ErrorBoundary';
-import EditNote from './Components/EditNote/EditNote';
 import axios from 'axios';
 const serverUrl = process.env.REACT_APP_SERVER_URL;
 
@@ -19,6 +15,7 @@ function AppF() {
   const [folders, setFolders] = useState([]);
   const [selectedFolder, setSelectedFolder] = useState(null);
   const [selectedNote, setSelectedNote] = useState(null);
+  const [error, setError] = useState(true);
 
   useEffect(() => {
     getData('folders');
@@ -55,13 +52,14 @@ function AppF() {
         }}
       >
         <Header title={'Noteful'} />
-        <ErrorBoundary>
           <Switch>
-            <Route exact path='/' component={FolderSelect} />
-            <Route path='/folder/:folderId' component={NoteRoute} />
-            <Route path='/add-folder' component={AddFolder} />
-            <Route path='/add-note' component={AddNote} />
-            <Route exact path='/edit-note/:note_id' component={EditNote} />
+            {routes.map((route) =>
+              route.exact ? (
+                <Route exact path={route.path} component={route.component} />
+              ) : (
+                <Route path={route.path} component={route.component} />
+              )
+            )}
             <Route
               path='/note'
               render={(routeProps) => (
@@ -71,9 +69,7 @@ function AppF() {
                 />
               )}
             />
-            <Route component={Error} />
           </Switch>
-        </ErrorBoundary>
       </NotefulContext.Provider>
     </BrowserRouter>
   );
