b610368eee88c07b19b890de7aadcd680d45e4fc
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-12-16 12:45:14 -0800

    login is working

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,60 +1,52 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+import styles from './Login.module.scss';
 import { useHistory } from 'react-router-dom';
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('email@test.com');
+  const [password, setPassword] = useState('password');
   const history = useHistory();
 
   async function submit(e) {
     e.preventDefault();
-    const url = `${process.env.SERVER}/login/getToken`;
-    const body = { email, password };
+    const url = `${process.env.REACT_APP_SERVER_URL}/api/login/gettoken`;
+    var data = { email, password };
     try {
-      const response = await axios.post(url, body);
-      localStorage.setItem('cool-jwt', response.data);
-      history.push('/protected');
-      return response;
+      let response = await axios.post(url, data);
+      console.log(response);
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
   }
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <div style={{ width: '50vw', height: '50vh', border: '1px solid blue' }}>
-        login form
-        <div>
-          <h5>Login</h5>
-          <form onSubmit={(e) => submit(e)}>
-            <label htmlFor='email'>
-              email
-              <input
-                type='text'
-                name='email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </label>
-            <label htmlFor='password'>
-              password
-              <input
-                type='password'
-                name='password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </label>
-            <button type='submit'>submit</button>
-          </form>
-        </div>
+    <div className={styles.viewport}>
+      <div className={styles.addForm}>
+        <h5>Login</h5>
+        <form onSubmit={(e) => submit(e)}>
+          <label htmlFor='email'>
+            email
+            <input
+              type='text'
+              name='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label htmlFor='password'>
+            password
+            <input
+              type='password'
+              name='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <button type='submit'>submit</button>
+        </form>
+        <Link to='/signup'>Don't have an account?</Link>
       </div>
     </div>
   );
